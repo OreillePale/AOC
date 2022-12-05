520e823b76dbd860a2c70a8917f2da0aae1e6fe8
etienne <etienne.batori@unine.ch>
2022-12-05 18:31:57 +0100

    cleaning

--- 2022/day1/target/debug/day1.d
+++
@@ -1,1 +0,0 @@
-C:\Users\Etienne\Documents\documents\AOC\2022\day1\target\debug\day1.exe: C:\Users\Etienne\Documents\documents\AOC\2022\day1\src\main.rs
--- 2022/day1/target/debug/deps/day1-3e3364b3c8f24c1b.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Etienne\Documents\documents\AOC\2022\day1\target\debug\deps\day1-3e3364b3c8f24c1b.rmeta: src\main.rs
-
-C:\Users\Etienne\Documents\documents\AOC\2022\day1\target\debug\deps\day1-3e3364b3c8f24c1b.d: src\main.rs
-
-src\main.rs:
